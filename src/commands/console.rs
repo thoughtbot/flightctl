bb5cd76dd3d9708e8f6dbe1bead1fa9240b17953
Joe Ferris <jferris@thoughtbot.com>
2021-10-15 10:15:18 -0400

    Reduce duplication in console command

--- src/commands/console.rs
+++ src/commands/console.rs
@@ -5,28 +5,13 @@ pub fn run_default(config: &Config, release: &Release) -> anyhow::Result<()> {
     let application = config.find_application(&release)?;
 
     match &application.config {
-        ApplicationConfig::Kubectl { console, selector } => {
-            let client = kubeclient::new(&release.context);
-            let base_selector = kubeclient::Selector::new(selector.clone());
-
-            match console {
-                Some(Console::Exec {
-                    command,
-                    container,
-                    selector,
-                }) => {
-                    let console_selector =
-                        base_selector.extend(&kubeclient::Selector::new(selector.clone()));
-                    let pod = client.get_available_pod(console_selector)?;
-                    pod.exec(&container, command)?;
-                    Ok(())
-                }
-                None => Err(anyhow::Error::msg(format!(
-                    "No console configured for application: {}",
-                    application.name
-                ))),
-            }
-        }
+        ApplicationConfig::Kubectl { console, .. } => match console {
+            Some(Console::Exec { command, .. }) => run_command(config, release, command),
+            None => Err(anyhow::Error::msg(format!(
+                "No console configured for application: {}",
+                application.name
+            ))),
+        },
     }
 }
 
